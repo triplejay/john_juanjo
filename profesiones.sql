38e1194faec979997534fe408b39bd2bf8b65046
john <john@iesiliberis.com>
2018-11-30 20:53:25 +0100

    Base de datos

---
+++ profesiones.sql
@@ -0,0 +1,50 @@
+-- MySQL dump 10.13  Distrib 5.6.42, for Linux (x86_64)
+--
+-- Host: localhost    Database: profesiones
+-- ------------------------------------------------------
+-- Server version	5.6.42
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `profesion`
+--
+
+DROP TABLE IF EXISTS `profesion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `profesion` (
+  `nombre` varchar(45) DEFAULT NULL,
+  `demandaPais` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `profesion`
+--
+
+LOCK TABLES `profesion` WRITE;
+/*!40000 ALTER TABLE `profesion` DISABLE KEYS */;
+INSERT INTO `profesion` VALUES ('ingeniero de software y desarrollador',24),('Ingeniero electrónico',19),('Ingeniero mecánico',19),('Enfermero ',18),('Médico ',16),('Ingeniero civil',16),('Ingeniero electrico',16),('analisis de datos',11),('contable',11),('director de ingeniería',9);
+/*!40000 ALTER TABLE `profesion` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-11-28 18:24:09
